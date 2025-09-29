564c74cc4895abda0572ce54e177262cb330a3c7
eblondel <emmanuel.blondel1@gmail.com>
2025-09-29 22:16:58 +0200

    update to R 4.5.1

--- global.R
+++ global.R
@@ -5,24 +5,42 @@ require(lubridate)
 require(stringr)
 
 #iotc software packages
+#remotes::install_github("iotc-secretariat/iotc-lib-core-utils-constants")
+require(iotc.core.utils.constants)
+#remotes::install_github("iotc-secretariat/iotc-lib-core-utils-aes")
+require(iotc.core.utils.aes)
+#remotes::install_github("iotc-secretariat/iotc-lib-core-utils-misc")
 require(iotc.core.utils.misc)
-require(iotc.core.gis.maps) #standalone
-require(iotc.core.gis.wkt) #standalone
-require(iotc.core.gis.cwp.IO) #standalone
+#remotes::install_github("iotc-secretariat/iotc-lib-core-gis-cwp-wkt", ref = "0.2.1-standalone")
+require(iotc.core.gis.wkt) 
+#remotes::install_github("iotc-secretariat/iotc-lib-core-gis-cwp")
+#remotes::install_github("iotc-secretariat/iotc-lib-core-gis-cwp-io", ref = "0.2.1-standalone")
+require(iotc.core.gis.cwp.IO)
+#remotes::install_github("iotc-secretariat/iotc-lib-core-gis-maps", ref = "0.2.1-standalone")
+require(iotc.core.gis.maps)
 
 #IOTC core db packages
+#remotes::install_github("iotc-secretariat/iotc-lib-core-db-connections")
 require(iotc.core.db.connections)
+#remotes::install_github("iotc-secretariat/iotc-lib-core-db-data")
 require(iotc.core.db.data)
 
 #IOTC base common packages
+#remotes::install_github("iotc-secretariat/iotc-lib-base-common-data")
 require(iotc.base.common.data)
+#remotes::install_github("iotc-secretariat/iotc-lib-base-common-plots")
 require(iotc.base.common.plots)
 
 #IOTC data packages
+#remotes::install_github("iotc-secretariat/iotc-data-reference-codelists")
 require(iotc.data.reference.codelists)
+#remotes::install_github("iotc-secretariat/iotc-reference-datasets-nc")
 require(iotc.data.reference.datasets.NC)
+#remotes::install_github("iotc-secretariat/iotc-reference-datasets-ce")
 require(iotc.data.reference.datasets.CE)
+#remotes::install_github("iotc-secretariat/iotc-reference-datasets-sf")
 require(iotc.data.reference.datasets.SF.raw)
+#remotes::install_github("iotc-secretariat/iotc-reference-datasets-sf-std")
 require(iotc.data.reference.datasets.SF.std)
 
 #global options
